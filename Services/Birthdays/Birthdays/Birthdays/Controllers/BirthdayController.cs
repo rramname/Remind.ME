240c1375b9ce885a98f3eed71394973fc6d036ef
rramname <rohit_ramname@hotmail.com>
2022-01-24 10:52:34 -0500

    logging and tests

--- Services/Birthdays/Birthdays/Birthdays/Controllers/BirthdayController.cs
+++ Services/Birthdays/Birthdays/Birthdays/Controllers/BirthdayController.cs
@@ -2,6 +2,7 @@
 using BirthdayService.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -12,21 +13,26 @@ namespace Birthdays.Controllers
     public class BirthdayController : ControllerBase
     {
         private readonly IBirthdayRepository _repo;
+        private readonly ILogger<BirthdayController> logger;
 
-        public BirthdayController(IBirthdayRepository repo)
+
+        public BirthdayController(IBirthdayRepository repo,ILogger<BirthdayController> _logger)
         {
             _repo = repo;
+            logger = _logger;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Birthday>>> GetAll()
         {
+            logger.LogInformation("Geting Birthdays API");
             return new ObjectResult(await _repo.GetAllUsers());
 
         }
         [HttpPost]
         public async Task<ActionResult<Birthday>> Create([FromBody] Birthday user)
         {
+            logger.LogInformation("Geting Birthdays API");
             await _repo.Create(user);
             return new OkObjectResult(user);
         }
